1740bae02be8386975bc32a394e68088eb2a1131
David La Grange <davidnlagrange@hotmail.com>
2021-04-06 16:05:31 -0500

    Changes for Deployment

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const collections = require('./routes/collections');
+const path = require('path');
 
 const app = express();
 
+
+
 connectDB();
 
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(express.json());
 app.use(cors());
 app.use('/api/collections', collections);
